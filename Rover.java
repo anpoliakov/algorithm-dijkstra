1423d185eba034052e38d79c2f765cfb6ae2b8c5
anpoliakov <Andrewlybimenko@gmail.com>
2021-05-17 19:39:29 +0300

    added a loop to analyze the distance to the last cell

--- Rover.java
+++ Rover.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Решение задачи алгоритмом Дейкстры
@@ -21,11 +18,49 @@ public class Rover {
         calculateRoverPath(args);
     }
 
+    //основной метод по заданию
     public static void calculateRoverPath(int[][] map) {
+        //инициализирую основные hashMap с которыми буду работать
         distance.createDistance(map);
         distance.replace("00", map[0][0]); //заполняем начальную точку default значением
         visited = createVisited(map);
         neigbors = scanNeighbors(map);
+
+        final int ROWS = map.length;
+        final int COLUMNS = map[0].length;
+        final String CELLFINISH = (rows-1) + "" + (columns-1);
+
+        //очередь обработки ячеек
+        Queue<String> cellsToProcess = new ArrayDeque<>();
+        cellsToProcess.offer("00");
+
+        //пока конечная точка карты не обработана (идём вперёд по алг. Дейкстры)
+        while(visited.get(CELLFINISH) != true){
+            String specificСell = cellsToProcess.poll(); //получаю ячейку из очереди на обработку
+            LinkedList<String> allNeighborsCell = neigbors.get(specificСell); //получаю список всех соседей конкретной ячейки
+            int distanceToCell = distance.get(specificСell); //дистанция от начальной точки до текущей
+
+            //проходим по всем соседям рассматриваемой ячеки
+            for(String neighbor : allNeighborsCell){
+                //если соседняя ячейка не рассмотрена - анализируем её соседей и расстояния до них
+                if(visited.get(neighbor) != true){
+                    int distanceToNeighbor = distanceToCell + getValueFromMap(neighbor);
+
+                    //если найден менее затратный путь - то distance до соседа перезаписываем
+                    if(distanceToNeighbor <= distance.get(neighbor)){
+                        distance.replace(neighbor,distanceToNeighbor);
+                    }
+
+                    //если в стеке ещё нет ячейки для дальнейшей обработки - добавляем
+                    if(!cellsToProcess.contains(neighbor)){
+                        cellsToProcess.add(neighbor);
+                    }
+                }
+            }
+
+            //говорим что рассматриваемая ячейка проверена и больше её трогать не нужно
+            visited.replace(specificСell, true);
+        }
     }
 
     //создаёт изначально заполненную бесконечным мнимыми значением hashMap
@@ -50,6 +85,7 @@ public class Rover {
         return visited;
     }
 
+    //состовляет списки соседей для каждой ячейки полученного массива
     private static HashMap<String,LinkedList<String>> scanNeighbors(int [][] map){
         HashMap<String,LinkedList<String>> neigbors = new HashMap<>();
         int rows = map.length;
